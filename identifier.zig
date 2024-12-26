5551911efb97c5fbdbeee44a95997b007a99b184
William Wong <williamw520@gmail.com>
2024-12-26 02:06:33 -0800

    More optimized v2

--- identifier.zig
+++ identifier.zig
@@ -7,20 +7,20 @@ const print = std.debug.print;
 const Vec32 = @Vector(32, u8);      // Assume SIMD-256 support with 32 byte vector.
 const Arr32 = *const [32]u8;        // Cast ptr to [32] array before cast to Vec32.
 
-fn nextBitmask32(base_ptr: [*]const u8, offset: usize) u32 {
-    const a32_chars = @as(Arr32, @ptrCast(base_ptr + offset)).*;
-    const v32_chars = @as(Vec32, a32_chars);
+fn nextBitmask32(raw_ptr: [*]const u8) u32 {
+    const arr32     = @as(Arr32, @ptrCast(raw_ptr)).*;
+    const vec32     = @as(Vec32, arr32);
 
-    const bit_A_Z   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('A')))) &
-                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('Z'))));
-    const bit_a_z   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('a')))) &
-                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('z'))));
-    const bit_u:u32 =          @bitCast(v32_chars == @as(Vec32, @splat('_')));
+    const bit_A_Z   = @as(u32, @bitCast(vec32 >= @as(Vec32, @splat('A')))) &
+                      @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('Z'))));
+    const bit_a_z   = @as(u32, @bitCast(vec32 >= @as(Vec32, @splat('a')))) &
+                      @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('z'))));
+    const bit_u:u32 =          @bitCast(vec32 == @as(Vec32, @splat('_')));
     const bit_azu   = bit_A_Z | bit_a_z | bit_u;
 
     // Digit should be in a separate function since leading char has different rules for digit.
-    const bit_0_9   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('0')))) &
-                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('9'))));
+    const bit_0_9   = @as(u32, @bitCast(vec32 >= @as(Vec32, @splat('0')))) &
+                      @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('9'))));
 
     print("bit_A_Z  {b:0>32}\n", .{ bit_A_Z });
     print("bit_a_z  {b:0>32}\n", .{ bit_a_z });
@@ -31,6 +31,19 @@ fn nextBitmask32(base_ptr: [*]const u8, offset: usize) u32 {
     return bit_azu | bit_0_9;
 }
 
+fn nextBitmask32b(raw_ptr: [*]const u8) u32 {
+    const arr32 = @as(Arr32, @ptrCast(raw_ptr)).*;
+    const vec32 = @as(Vec32, arr32);
+    return
+        (@as(u32, @bitCast(vec32 >= @as(Vec32, @splat('A')))) &
+         @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('Z'))))) |
+        (@as(u32, @bitCast(vec32 >= @as(Vec32, @splat('a')))) &
+         @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('z'))))) |
+        (@as(u32, @bitCast(vec32 == @as(Vec32, @splat('_'))))) |
+        (@as(u32, @bitCast(vec32 >= @as(Vec32, @splat('0')))) &
+         @as(u32, @bitCast(vec32 <= @as(Vec32, @splat('9')))));
+}
+
 pub fn main() !void {
     const text = "..ABcd..xy_z.._1209.. ```~~()...|....(_Efg123)....~~.##$%........|.......";
     print("text: {s}\n\n", .{ text });
@@ -39,7 +52,8 @@ pub fn main() !void {
     const size = bytes.len/32 * 32;     // handling remainder text is left as an exercise
     var offset: usize = 0;
     while (offset < size) : (offset += 32) {
-        var mask = nextBitmask32(bytes.ptr, offset);
+        var mask = nextBitmask32(bytes.ptr + offset);
+        //var mask = nextBitmask32b(bytes.ptr + offset);
         print("  mask:  {b:0>32}\n", .{ mask });
 
         // Dump the identifiers found.
