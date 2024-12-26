fa6c72f2761303ab399b8168d6f5725031e00b5c
William Wong <williamw520@gmail.com>
2024-12-25 22:29:50 -0800

    Reduce generated asm opcodes.

--- identifier.zig
+++ identifier.zig
@@ -6,48 +6,32 @@ const print = std.debug.print;
 
 const Vec32 = @Vector(32, u8);      // Assume SIMD-256 support with 32 byte vector.
 const Arr32 = *const [32]u8;        // Cast ptr to [32] array before cast to Vec32.
-const v32_0: Vec32 = @splat('0');   // Fill SIMD vecs with the limit chars.
-const v32_9: Vec32 = @splat('9');
-const v32_A: Vec32 = @splat('A');
-const v32_Z: Vec32 = @splat('Z');
-const v32_a: Vec32 = @splat('a');
-const v32_z: Vec32 = @splat('z');
-const v32_us: Vec32 = @splat('_');
 
 fn nextBitmask32(base_ptr: [*]const u8, offset: usize) u32 {
     const a32_chars = @as(Arr32, @ptrCast(base_ptr + offset)).*;
     const v32_chars = @as(Vec32, a32_chars);
-    const bit_ge_A: u32 = @bitCast(v32_chars >= v32_A);
-    const bit_le_Z: u32 = @bitCast(v32_chars <= v32_Z);
-    const bit_ge_a: u32 = @bitCast(v32_chars >= v32_a);
-    const bit_le_z: u32 = @bitCast(v32_chars <= v32_z);
-    const bit_us:   u32 = @bitCast(v32_chars == v32_us);
-    const bit_A_Z:  u32 = bit_ge_A & bit_le_Z;
-    const bit_a_z:  u32 = bit_ge_a & bit_le_z;
-    const alphas:   u32 = bit_A_Z | bit_a_z | bit_us;
+
+    const bit_A_Z   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('A')))) &
+                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('Z'))));
+    const bit_a_z   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('a')))) &
+                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('z'))));
+    const bit_u:u32 =          @bitCast(v32_chars == @as(Vec32, @splat('_')));
+    const bit_azu   = bit_A_Z | bit_a_z | bit_u;
 
     // Digit should be in a separate function since leading char has different rules for digit.
-    const bit_ge_0: u32 = @bitCast(v32_chars >= v32_0);
-    const bit_le_9: u32 = @bitCast(v32_chars <= v32_9);
-    const digits:   u32 = bit_ge_0 & bit_le_9;
+    const bit_0_9   = @as(u32, @bitCast(v32_chars >= @as(Vec32, @splat('0')))) &
+                      @as(u32, @bitCast(v32_chars <= @as(Vec32, @splat('9'))));
 
-    print("bit_ge_A {b:0>32}\n", .{ bit_ge_A });
-    print("bit_le_Z {b:0>32}\n", .{ bit_le_Z });
-    print("bit_ge_a {b:0>32}\n", .{ bit_ge_a });
-    print("bit_le_z {b:0>32}\n", .{ bit_le_z });
     print("bit_A_Z  {b:0>32}\n", .{ bit_A_Z });
     print("bit_a_z  {b:0>32}\n", .{ bit_a_z });
-    print("alphas   {b:0>32}\n", .{ alphas });
-    print("bit_ge_0 {b:0>32}\n", .{ bit_ge_0 });
-    print("bit_le_9 {b:0>32}\n", .{ bit_le_9 });
-    print("digits   {b:0>32}\n", .{ digits });
+    print("alphas   {b:0>32}\n", .{ bit_azu });
+    print("digits   {b:0>32}\n", .{ bit_0_9 });
 
-    const bitmask:  u32 = alphas | digits;
-    return bitmask;
+    return bit_azu | bit_0_9;
 }
 
 pub fn main() !void {
-    const text = "..ABcd..xy_z.._1209.. ```~~()...|....(_ef123)....~~.##$%........|.......";
+    const text = "..ABcd..xy_z.._1209.. ```~~()...|....(_Efg123)....~~.##$%........|.......";
     print("text: {s}\n\n", .{ text });
 
     const bytes = @as([] const u8, text);
