e2fb43de5ad5bef6a835c78bc977a09541388768
William Wong <williamw520@gmail.com>
2025-12-26 17:17:27 -0800

    Update http_get for zig 0.15.1.

--- http_get.zig
+++ http_get.zig
@@ -5,30 +5,45 @@ const std = @import("std");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-    const res = try get(allocator, "https://example.com");
-    defer allocator.free(res);
-    std.debug.print("{s}\n", .{res});
-}
+    const alloc = gpa.allocator();
 
-fn get(allocator: std.mem.Allocator, a: []const u8) ![]const u8 {
-    const uri = try std.Uri.parse(a);
-    var client = std.http.Client{ .allocator = allocator };
+    var client = std.http.Client{ .allocator = alloc };
     defer client.deinit();
 
-    const server_header_buffer: []u8 = try allocator.alloc(u8, 1024 * 8);
-    defer allocator.free(server_header_buffer);
+    var response = std.Io.Writer.Allocating.init(alloc);
+    defer response.deinit();
 
-    var req = try client.open(.GET, uri, .{
-        .server_header_buffer = server_header_buffer,
+    const result1 = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .uri = try std.Uri.parse("https://example.com") },
+        .response_writer = &response.writer,
     });
-    defer req.deinit();
+    std.debug.print("\nHTTP Status: {}\n{s}\n", .{result1.status, response.written()});
+    response.clearRetainingCapacity();
 
-    try req.send();
-    try req.finish();
-    try req.wait();
+    const result2 = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .uri = try std.Uri.parse("https://httpbin.org/get?abc=1&xyz=2") },
+        .response_writer = &response.writer,
+    });
+    std.debug.print("\nHTTP Status: {}\n{s}\n", .{result2.status, response.written()});
 
-    const body = try req.reader().readAllAlloc(allocator, 300000 * 8);
-    return body;
+    // const response1 = try httpGet(alloc, "https://example.com");
+    // defer alloc.free(response1);
+    // std.debug.print("{s}\n", .{response1});
 }
+
+fn httpGet(alloc: std.mem.Allocator, url: []const u8) ![]const u8 {
+    var client = std.http.Client{ .allocator = alloc };
+    defer client.deinit();
+
+    var body_writer = std.Io.Writer.Allocating.init(alloc);
+    _ = try client.fetch(.{
+        .method = .GET,
+        .location = .{ .uri = try std.Uri.parse(url) },
+        .response_writer = &body_writer.writer,
+    });
+    return body_writer.toOwnedSlice();
+}
+
 
